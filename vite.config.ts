1970587d11408ae30b3d6dd357b43e1ceabfa512
iobi <iobi@ecobank.com>
2026-03-21 18:43:06 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env.NODE_ENV': '"production"',
+  },
   build: {
     lib: {
       entry: "src/widget.tsx",
